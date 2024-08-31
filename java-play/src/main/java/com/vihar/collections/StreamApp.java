cbfc6ee05248429f14fdda631a70ee0a97c9725d
balavihar <pbalavihar@gmail.com>
2024-09-01 02:09:01 +0530

    Added first vertx app.

---
+++ java-play/src/main/java/com/vihar/collections/StreamApp.java
@@ -0,0 +1,10 @@
+package com.vihar.collections;
+
+public class StreamApp {
+
+    public static void main(String[] args) {
+        System.out.println("Java 11 Collection Examples.");
+        String str1 = "123456";
+        System.out.println(System.getProperty("java.version"));
+    }
+}
