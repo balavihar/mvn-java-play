d59e7ad126ed0a8fd16606953b25ae9d4e4c3b65
balavihar <pbalavihar@gmail.com>
2025-02-06 00:27:01 +0530

    foreach play

--- vertx-starter/src/test/java/com/vihar/AppTest.java
+++ vertx-starter/src/test/java/com/vihar/AppTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Unit test for simple App.
  */
@@ -15,6 +18,12 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
+        List<String> fruits = Arrays.asList("a", "m", "b", "o", "g", "p", "r", "h");
+        fruits.forEach(System.out::print);
+        System.out.println();
+        fruits.stream().parallel().forEach(System.out::print);
+        System.out.println();
+        fruits.stream().parallel().forEachOrdered(System.out::print);
         assertTrue( true );
     }
 }
